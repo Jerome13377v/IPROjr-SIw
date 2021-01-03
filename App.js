f6f57fa7d996f394e046e6f8ce95a2d89c579576
Jerome13377v <asd553622045@sina.com>
2021-01-04 05:12:39 +0800

    Logo Da IPRO

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import iprojr from './assets/ipro.png';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+
+        <Image source={require('./assets/ipro.png')}
+          style={{ width: 400, height: 400 }}
+        />
+      </View>
+      <Text>AHHH MLK</Text>
       <StatusBar style="auto" />
     </View>
   );
