91be6c5ddf61036f93bc9370fd50224fe4b62fe4
Jerome13377v <asd553622045@sina.com>
2021-04-18 20:23:33 +0800

    Adicionados Formulários finais de Bug e FeedBack

--- src/pages/project-forms/ProjectForms.js
+++ src/pages/project-forms/ProjectForms.js
@@ -6,7 +6,7 @@ export class Bugs extends Component {
     return (
       <WebView
         source={{
-          uri: 'https://docs.google.com/forms/d/e/1FAIpQLSfMF3wW_J-hAKTyQFdKDSyJPAPx3Oqr_-DlzFMWgT0ku75kDA/viewform?usp=sf_link'
+          uri: 'https://docs.google.com/forms/d/e/1FAIpQLSezxm9-I5SjUxBjbaTOC7adXKmOKMlNXGyn6I2aijfW1D78sQ/viewform?usp=sf_link'
         }}
         style={{ marginTop: 20 }}
       />
@@ -19,7 +19,7 @@ export class FeedBack extends Component {
       return (
         <WebView
           source={{
-            uri: 'https://docs.google.com/forms/d/e/1FAIpQLSfMF3wW_J-hAKTyQFdKDSyJPAPx3Oqr_-DlzFMWgT0ku75kDA/viewform?usp=sf_link'
+            uri: 'https://docs.google.com/forms/d/e/1FAIpQLScHXRZYNtLkMtyzYsNX-kqxthC5EJOg3uHhcFy2muIYHrmesg/viewform?usp=sf_link'
           }}
           style={{ marginTop: 20 }}
         />
