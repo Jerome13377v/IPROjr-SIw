5a08552f5f52ae5317ade08e90fa66e1dd7b8cc4
Jerome13377v <asd553622045@sina.com>
2021-05-22 17:45:30 +0800

    test failed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,13 +8,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5tmpVHFUIDW7nq87eJv57BgKW54G5iwI",
-  authDomain: "ipro-database.firebaseapp.com",
-  projectId: "ipro-database",
-  storageBucket: "ipro-database.appspot.com",
-  messagingSenderId: "649052552256",
-  appId: "1:649052552256:web:30aae1b3568760fd74b3a5",
-  measurementId: "G-VLVDDEQTWL"
+  apiKey: "========================================",
+  authDomain: "==============================",
+  projectId: "================================",
+  storageBucket: "=============================",
+  messagingSenderId: "===========================",
+  appId: "========================================",
+  measurementId: "=================="
 };
 
 // Initialize Firebase
