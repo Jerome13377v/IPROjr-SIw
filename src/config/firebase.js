4441c16a89848c8339e3c4c16721e269a7a360fc
Jerome13377v <asd553622045@sina.com>
2021-05-31 21:14:25 +0800

    security issue solved

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,18 +4,11 @@ import firebase from "firebase/app";
 import 'firebase/storage';
 import "firebase/auth";
 import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../API_KEY'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "========================================",
-  authDomain: "==============================",
-  projectId: "================================",
-  storageBucket: "=============================",
-  messagingSenderId: "===========================",
-  appId: "========================================",
-  measurementId: "=================="
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
